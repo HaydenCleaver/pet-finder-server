a638f5f18351b3f0d5ecf279a61738351eef1596
Hayden Cleaver <rexcleaver90@gmail.com>
2022-08-02 23:53:38 -0400

    server.js updated with module calls

--- server.js
+++ server.js
@@ -5,19 +5,28 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require ('mongoose');
 
+const handlePets = require('./pets');
+const postFavorite = require('./modules/postFavorite.js');
+const getFavorite = require('./modules/getFavorite.js');
+const deleteFavorite = require('./modules/deleteFavorite.js');
 const app = express();
 app.use(cors());
 
 mongoose.connect(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3001;
-const handlePets = require('./pets');
 
 app.use(express.json());
 
 app.get('/pets', (request, response) => {
   handlePets(request, response);
 });
+
+app.post('/pets', postFavorite);
+
+app.get('/pets', getFavorite);
+
+app.delete('pets/:id', deleteFavorite);
 
 app.use('*', (error, request, response, next) => {
   response.status(500).send(error);
